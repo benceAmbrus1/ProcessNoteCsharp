5dff93603603798a3f0c525b4fb9bed37d282960
benceAmbrus1 <o.g.bence@gmail.com>
2018-07-05 15:08:04 +0200

    procesNote 1st version done

--- ProcessNoteApp/Form1.Designer.cs
+++ ProcessNoteApp/Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(370, 407);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(560, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(683, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(147, 425);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(452, 28);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(225, 200);
+            this.treeView1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(452, 399);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(683, 399);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 20);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(452, 262);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(306, 134);
+            this.listBox2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(573, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Notes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ProcessNoteApp/Form1.cs
+++ ProcessNoteApp/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,106 @@ namespace ProcessNoteApp
 {
     public partial class Form1 : Form
     {
+        private Process[] processes;
+        private PerformanceCounter theCPUCounter;
+        private Process p;
+        private List<String> messages = new List<String>();
+            
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            processes = System.Diagnostics.Process.GetProcesses();
+            Array.Sort(processes,
+            delegate (System.Diagnostics.Process x, System.Diagnostics.Process y) { return x.ProcessName.CompareTo(y.ProcessName); });
+
+            foreach (System.Diagnostics.Process process in processes)
+            {
+                listBox1.Items.Add("PID: " + process.Id + ": " + process.ProcessName);
+
+            }
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                button1.Visible = true;
+                button1.Click += new EventHandler(delete_clicked);
+                treeView1.Nodes.Clear();
+                p = processes[listBox1.SelectedIndex];
+
+                theCPUCounter = new PerformanceCounter();
+                label1.Text = p.ProcessName;
+                theCPUCounter.CategoryName = "Process";
+                theCPUCounter.CounterName = "% Processor Time";
+                theCPUCounter.InstanceName = p.ProcessName;
+                TreeNode treeNode = new TreeNode("CPU usage: " + p.TotalProcessorTime);
+                TreeNode treeNode2 = new TreeNode("Memory usage: " + (p.WorkingSet64 / (1024*1024)) + " MB");
+                TreeNode treeNode3 = new TreeNode("Running time: " + (DateTime.Now - p.StartTime));
+                TreeNode treeNode4 = new TreeNode("Start time: " + p.StartTime);
+                List<TreeNode> list = new List<TreeNode>();
+                foreach (ProcessThread t in p.Threads)
+                {
+                    TreeNode tempThreadNode = new TreeNode("Thread ID: " + t.Id);
+                    list.Add(tempThreadNode);
+                }
+                TreeNode treeNode5 = new TreeNode("Threads", list.ToArray());
+
+                treeView1.Nodes.Add(treeNode);
+                treeView1.Nodes.Add(treeNode2);
+                treeView1.Nodes.Add(treeNode3);
+                treeView1.Nodes.Add(treeNode4);
+                treeView1.Nodes.Add(treeNode5);
+
+            }
+            catch (Exception)
+            {
+                TreeNode treeNode = new TreeNode("Access Denied");
+                treeView1.Nodes.Add(treeNode);
+            }
+        }
+
+        private void delete_clicked(object sender, EventArgs e)
+        {
+            processes[listBox1.SelectedIndex].Kill();
+            listBox1.Items.Clear();
+            processes = System.Diagnostics.Process.GetProcesses();
+            Array.Sort(processes, delegate (System.Diagnostics.Process x, System.Diagnostics.Process y) { return x.ProcessName.CompareTo(y.ProcessName); });
+            foreach (System.Diagnostics.Process process in processes)
+            {
+                listBox1.Items.Add("PID: " + process.Id + ": " + process.ProcessName);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            processes = System.Diagnostics.Process.GetProcesses();
+            Array.Sort(processes,
+            delegate (System.Diagnostics.Process x, System.Diagnostics.Process y) { return x.ProcessName.CompareTo(y.ProcessName); });
+
+            foreach (System.Diagnostics.Process process in processes)
+            {
+                listBox1.Items.Add("PID: " + process.Id + ": " + process.ProcessName);
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(textBox1.Text))
+            {
+                String message = DateTime.Now + " | " + textBox1.Text;
+                messages.Add(message);
+                listBox2.Items.Add(message);
+                textBox1.Clear();
+            }
         }
     }
 }
